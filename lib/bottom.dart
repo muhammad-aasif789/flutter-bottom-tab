f41f890641a19e5c124f03e9b5f4fac6b724789c
muhammad-asif789 <muhammadasifjagirani789@gmail.com>
2021-11-17 11:35:04 -0800

    first commit

---
+++ lib/bottom.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bottom_tab/screen1.dart';
+import 'package:flutter_bottom_tab/screen2.dart';
+import 'package:flutter_bottom_tab/screen3.dart';
+import 'package:flutter_bottom_tab/screen4.dart';
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int currentTab = 0;
+
+  Widget currentScreen = Screen1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: PageStorage(bucket: PageStorageBucket(), child: currentScreen),
+      bottomNavigationBar: BottomAppBar(
+        // notchMargin: 50,
+        color: Colors.red[900],
+        child: Container(
+          height: 60,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                MaterialButton(
+                    minWidth: 50,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen1();
+                        currentTab = 0;
+                      });
+                    },
+                    child: Icon(Icons.home,
+                        color: currentTab == 0 ? Colors.white : Colors.black)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen2();
+                        currentTab = 1;
+                      });
+                    },
+                    child: Icon(Icons.favorite,
+                        color: currentTab == 1 ? Colors.white : Colors.black)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen3();
+                        currentTab = 2;
+                      });
+                    },
+                    child: Icon(Icons.search,
+                        color: currentTab == 2 ? Colors.white : Colors.black)),
+                MaterialButton(
+                    minWidth: 60,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Screen4();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Icon(Icons.person,
+                        color: currentTab == 3 ? Colors.white : Colors.black)),
+              ]),
+        ),
+      ),
+    );
+  }
+}
